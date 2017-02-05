12e336d983692c153447f1e6a0e473342f797992
Ravinder1289 <Ravinderkaur1289@gmail.com>
2017-02-05 23:14:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+//change the text of the main-text div:
+var element = document.getElementById('main-text');
+element.innerHtml="New Value";
+
+//Move the image:
+var img=document.getElementById('madi');
+img.onclick = function() {
+    img.style.leftmarginLeft='50px'
+};
